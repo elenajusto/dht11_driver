3ac5beb4ed64c7d700cdfcdb42d5681affdc578b
Elena <elenajusto@users.noreply.github.com>
2024-03-16 12:08:58 +1100

    Initial file structure

---
+++ src/main.c
@@ -0,0 +1,37 @@
+/* 
+*   Program Name:   Driver for DHT-11 Temperature and Humidity Sensor
+*         Author:   Elena Justo
+*
+*/
+
+
+/* Header Files */
+
+
+
+/* Variables */
+
+
+
+/* Function Prototypes */
+
+
+
+/* Main Program Function */ 
+int main(void){
+
+    /* Main Program Loop */
+    while (1){
+
+
+
+    };
+
+    return (0);
+}
+
+
+/* Function Definitions */
+
+
+
