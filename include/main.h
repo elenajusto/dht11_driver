eb890b1fcb696e562ad7b55a3cc59b4ddbe57f38
Elena <elenajusto@users.noreply.github.com>
2024-03-26 16:16:03 +1100

    Cleaning up formatting and code

---
+++ include/main.h
@@ -0,0 +1,12 @@
+/*
+*
+*       TEST PROGRAM HEADER FROM PLATFORM.IO DOCUMENTATION TO VERIFY EVERYTHING WORKS.
+*
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stm32g0xx_hal.h"
+
+#endif // MAIN_H
--- src/main.c
+++ src/main.c
@@ -4,31 +4,45 @@
 *
 */
 
-
 /* Header Files */
-
-
+#include <main.h>
 
 /* Variables */
-
-
 
 /* Function Prototypes */
 
 
+/* HAL Prototypes */
+static void MX_GPIO_Init(void);
 
 /* Main Program Function */ 
 int main(void){
 
     /* Main Program Loop */
     while (1){
-    };
+    }
 
     return (0);
 }
 
 
-/* Function Definitions */
+/* GPIO Initialisation */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOI_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  HAL_PWREx_EnableVddIO2();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
 
-
-
+  /*Configure GPIO pin : DHT-11 Signal Pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
